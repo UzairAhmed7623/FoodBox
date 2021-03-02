9b0edd6efbc80ed79ba4789ff566c0b5412710c4
UzairAhmed7623 <hafiz.uzair579@gmail.com>
2021-03-02 17:33:39 +0500

    Commit 1!

--- app/src/main/java/com/example/foodbox/SearchActivity.java
+++ app/src/main/java/com/example/foodbox/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.foodbox;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.provider.SettingsSlicesContract;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,9 +21,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.foodbox.adapters.SearchAdapter;
+import com.example.foodbox.models.ItemsModelClass;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -30,6 +40,7 @@ public class SearchActivity extends AppCompatActivity {
     private RecyclerView rvSearch;
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore firebaseFirestore;
+    private ArrayList<ItemsModelClass> items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +53,71 @@ public class SearchActivity extends AppCompatActivity {
         etSearch = (EditText) findViewById(R.id.etSearch);
         btnSearch = (Button) findViewById(R.id.btnSearch);
         rvSearch = (RecyclerView) findViewById(R.id.rvSearch);
+        rvSearch.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
 
-        btnSearch.setOnClickListener(new View.OnClickListener() {
+        searchData();
+
+        etSearch.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (etSearch.getText().toString().equals("")){
+                    rvSearch.setVisibility(View.GONE);
+                }
+                else {
+                    rvSearch.setVisibility(View.VISIBLE);
+                    filter(s.toString());
+                }
+            }
+        });
+    }
 
-                String text = etSearch.getText().toString().trim();
+    private void filter(String text) {
+        ArrayList<ItemsModelClass> filterdList = new ArrayList<>();
+        for (ItemsModelClass items : items){
+            if (items.getItemName().toLowerCase().contains(text.toLowerCase())){
+                filterdList.add(items);
+            }
+        }
 
-                firebaseFirestore.collection("Restaurants").whereEqualTo("", "")
+        rvSearch.setAdapter(new SearchAdapter(SearchActivity.this, filterdList));
+
+    }
+
+    private void searchData() {
+        firebaseFirestore.collection("Restaurants").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    for (QueryDocumentSnapshot documentSnapshot : task.getResult()){
+                        String resName = documentSnapshot.getId();
+                        firebaseFirestore.collection("Restaurants").document(resName)
+                                .collection("Items").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()){
+                                    for (QueryDocumentSnapshot documentSnapshot1 : task.getResult()){
+                                        String itemName = documentSnapshot1.getId();
+                                        String price = documentSnapshot1.getString("price");
+                                        String imageUri = documentSnapshot1.getString("imageUri");
+
+                                        ItemsModelClass itemsModelClass = new ItemsModelClass();
+                                        itemsModelClass.setItemName(itemName);
+                                        itemsModelClass.setPrice(price);
+                                        itemsModelClass.setImageUri(imageUri);
+
+                                        items.add(itemsModelClass);
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
 
             }
         });
