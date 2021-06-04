f63ff217febfd218c3acf1ed09d53327c2e9c5f6
UzairAhmed7623 <hafiz.uzair579@gmail.com>
2021-06-04 16:43:22 +0500

    Commit 11!

--- app/src/main/java/com/inkhornsolutions/foodbox/Checkout.java
+++ app/src/main/java/com/inkhornsolutions/foodbox/Checkout.java
@@ -3,6 +3,7 @@ package com.inkhornsolutions.foodbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Checkout extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Checkout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
     }
 }
