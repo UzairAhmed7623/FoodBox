9e16dfee8d7cd8fb4bf2b59817d7fe4cc8274a70
UzairAhmed7623 <hafiz.uzair579@gmail.com>
2021-08-13 18:10:44 +0500

    Commit 12!

--- app/src/main/java/com/inkhornsolutions/foodbox/SignUp.java
+++ app/src/main/java/com/inkhornsolutions/foodbox/SignUp.java
@@ -2,14 +2,19 @@ package com.inkhornsolutions.foodbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
 
 public class SignUp extends AppCompatActivity {
 
@@ -22,10 +27,11 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         etNumber = (EditText) findViewById(R.id.etNumber);
         btnSignUp = (MaterialButton) findViewById(R.id.btnSignUp);
         textInputLayout = (TextInputLayout) findViewById(R.id.textInputLayout);
-
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
